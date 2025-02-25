783b12efc1b45c9cdf6d12338387136e7eb99a1a
Karl Ervine <karl.ervine@rmit.edu.au>
2025-02-26 07:54:14 +1100

    no message

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-
+  site: 'https://learninglab.rmit.edu.au/AssignmentPlanner',
+  base: '/AssignmentPlanner/',
   trailingSlash: 'always',
 
   vite: {
