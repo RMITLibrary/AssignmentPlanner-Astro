9d80297fb43644febbd84345de0c0a5c04a9732d
Karl Ervine <karl.ervine@rmit.edu.au>
2025-02-27 17:19:57 +1100

    hotfix: hack to get to deploy currently

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://learninglab.rmit.edu.au',
-  base: '/AssignmentPlanner',
+  site: 'https://learninglab.rmit.edu.au/AssignmentPlanner',
+
   trailingSlash: 'always',
   vite: {
     logLevel: 'error', // Options: 'info', 'warn', 'error', 'silent'
