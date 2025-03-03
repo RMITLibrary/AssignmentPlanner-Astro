afcdc62b16c19b30bf4002807223fe021c303718
Karl Ervine <karl.ervine@rmit.edu.au>
2025-03-03 11:22:40 +1100

    feat: netlify env production update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
-
 import preact from '@astrojs/preact';
+
+// Determine the base path based on environment variable
+const isSubdirectoryDeployment = process.env.DEPLOY_SUBDIRECTORY === 'true';
+const basePath = isSubdirectoryDeployment ? '/AssignmentPlanner/' : '/';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://learninglab.rmit.edu.au',
-  base: '/AssignmentPlanner/',
+  site: 'https://learninglab.rmit.edu.au', // This is your canonical site URL
+  base: basePath, // Use the conditional base path
 
   trailingSlash: 'always',
   vite: {
-    logLevel: 'error', // Options: 'info', 'warn', 'error', 'silent'
+    logLevel: 'error',
   },
-
   integrations: [sitemap(), preact()],
 });
