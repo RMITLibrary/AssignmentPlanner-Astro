060c17fdc7a6d27e1649ec5f12cb15a0989ac909
Karl Ervine <karl.ervine@rmit.edu.au>
2025-03-11 15:03:20 +1100

    hack: quickly switch to calendar tab then back on print so calendar renders

--- src/components/SaveToPdfButton.jsx
+++ src/components/SaveToPdfButton.jsx
@@ -1,14 +1,37 @@
 // src/components/SaveToPdfButton.jsx
 import { h } from 'preact';
-import { fireDataLayerEvent } from '../utils'; // Import the common function
+import { activeTabStore } from '../store';
+import { fireDataLayerEvent } from '../utils';
 
 const SaveToPdfButton = () => {
+  const changeTab = (tab) => {
+    activeTabStore.set(tab);
+  };
+
   const handlePrintPDF = () => {
-    fireDataLayerEvent({
-      event: 'pdf_print',
-      formType: 'assignment_planner',
-    });
-    window.print(); // Trigger the print dialog
+    // Store the current active tab
+    const currentTab = activeTabStore.get();
+
+    // Switch to calendar view if not already active //hack to show calendar in print
+    if (currentTab !== 'calendar') {
+      changeTab('calendar');
+    }
+
+    // Wait for the calendar view to render
+    setTimeout(() => {
+      // Data Layer Push for PDF Print
+      fireDataLayerEvent({
+        event: 'pdf_print',
+        formType: 'assignment_planner',
+      });
+
+      window.print(); // Trigger the print dialog
+
+      // Switch back to the original tab after printing (if not already active) //hack to show calendar in print
+      if (currentTab !== 'calendar') {
+        changeTab(currentTab);
+      }
+    }, 100); // Using a timeout of 0 to execute after the current call stack
   };
 
   return (
